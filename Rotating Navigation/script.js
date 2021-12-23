f3955cc203f3405fee98b6cf94e93f76c9e0caa2
elhajuojy <souirimehdi311@gmail.com>
2021-12-23 12:23:55 +0100

    some update

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -3,7 +3,7 @@ const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
 
-open.addEvenetListener('click',()=>container.classList.add('show_nav'))
+open.addEvenetListener('click',() => container.classList.add('show_nav'))
 
 
 close.addEvenetListener('click',()=>container.classList.remove('show_nav'))
